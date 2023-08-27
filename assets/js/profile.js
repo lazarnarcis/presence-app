b0c58340cfea28e1bf26d6096f5a8a515864d260
Narcis Lazar <lnarcis310@gmail.com>
2023-08-27 16:12:20 +0300

    remove console log from edit user click

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -25,7 +25,6 @@ $(document).ready(function() {
             type: "POST",
             data: $("#form_edit_user").serialize(),
             success: function (data) {
-                console.log(data);
                 if (data == 1) {
                     Swal.fire({
                         title: "Are you sure?",
