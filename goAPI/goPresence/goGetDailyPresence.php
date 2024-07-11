038a2c25b57841660557086f16614ca8047b1512
Lazar Narcis <lnarcis310@gmail.com>
2024-07-11 10:01:02 +0000

    fix bug in show presence for users

--- goAPI/goPresence/goGetDailyPresence.php
+++ goAPI/goPresence/goGetDailyPresence.php
@@ -11,14 +11,35 @@
     if ($username != "") {
         $where_string .= " AND users.name LIKE '%$username%' ";
     }
-    $query = "SELECT presence.user_id, presence.date, users.name, presence.seconds FROM presence LEFT JOIN users ON users.id=presence.user_id WHERE presence.date BETWEEN '$start_date' AND '$end_date' $where_string GROUP BY users.id, presence.date ORDER BY presence.date";
+
+    $start_date_sec = strtotime($start_date);
+    $end_date_sec = strtotime($end_date);
+    $end_date_sec += 86399;
+    
+    $query = "SELECT 
+        vp.user_id,
+        DATE(FROM_UNIXTIME(vp.leave_time)) AS date,
+        vp.username AS name,
+        SUM(vp.total_time) AS seconds
+    FROM 
+        voice_presence vp
+    LEFT JOIN 
+        users u ON u.id = vp.user_id 
+    WHERE 
+        vp.join_time >= $start_date_sec
+        AND vp.leave_time <= $end_date_sec 
+        $where_string
+    GROUP BY 
+        vp.user_id, DATE(FROM_UNIXTIME(vp.leave_time))
+    ORDER BY 
+        DATE(FROM_UNIXTIME(vp.leave_time));
+    ";
     $presence = $db->query($query);
 
     $dataID = [];
     $dataDate = [];
     $dataName = [];
     $dataSeconds = [];
-    
     foreach ($presence as $user) {
         $dataID[] = $user['user_id'];
         $dataDate[] = $user['date'];
--- goAPI/goPresence/goGetMonthlyPresence.php
+++ goAPI/goPresence/goGetMonthlyPresence.php
@@ -11,7 +11,28 @@
     if ($username != "") {
         $where_string .= " AND users.name LIKE '%$username%' ";
     }
-    $query = "SELECT presence.user_id, DATE_FORMAT(presence.date, '%Y-%m') AS month, users.name, SUM(presence.seconds) as seconds FROM presence LEFT JOIN users ON users.id=presence.user_id WHERE presence.date >= '$start_date' AND presence.date <= '$end_date' $where_string GROUP BY presence.user_id, month ORDER BY month;";
+
+    $start_date_sec = strtotime($start_date);
+    $end_date_sec = strtotime($end_date);
+    $end_date_sec += 86399;
+
+    $query = "SELECT 
+        vp.user_id,
+        DATE_FORMAT(FROM_UNIXTIME(vp.leave_time), '%Y-%m') AS month,
+        vp.username AS name,
+        SUM(vp.total_time) AS seconds
+    FROM 
+        voice_presence vp
+    LEFT JOIN 
+        users u ON u.id = vp.user_id 
+    WHERE 
+        vp.join_time >= $start_date_sec
+        AND vp.leave_time <= $end_date_sec 
+        $where_string
+    GROUP BY 
+        vp.user_id, month
+    ORDER BY 
+        month;";
     $presence = $db->query($query);
 
     $dataID = [];
