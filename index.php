4aacad716edd037f310f3ca6f2f25293483322e1
root <root@vps-bda90107.vps.ovh.net>
2024-10-01 19:28:56 +0000

    add news info on welcome page

--- index.php
+++ index.php
@@ -54,11 +54,6 @@
 		<div class="heading">
 			<h1 id="text-news"></h1>			
 			<h3 class="cd-headline clip is-full-width">
-				<span>Here you can see your </span>
-				<span class="cd-words-wrapper">
-					<b class="is-visible">daily presence.</b>
-					<b>monthly presence.</b>
-				</span>
 				<div class="btn-ber">
 					<a class="get_btn hvr-bounce-to-top" href="daily-presence.php">See the presence</a>
 				</div>
